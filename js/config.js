da4ce4fa54aa12435363c860add059e9f6a3dc3d
keWiinn <147539611+keWiinn@users.noreply.github.com>
2024-09-24 12:17:34 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
   cognito: {
     userPoolId: "us-west-2_mzwZ0fTVp" ,// "us-west-2_Gl1gpDfZt" 
-    userPoolClientId: "2rapbgh1con1a8etblg7ei3jq3" //48dnc3fu1rjsg8lifvpms7cslm",
+    userPoolClientId: "2rapbgh1con1a8etblg7ei3jq3", //48dnc3fu1rjsg8lifvpms7cslm",
     region: "us-west-2"
   },
   api: {
